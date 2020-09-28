f9fa5dc8bf8ca0afecada6818b6c0ac89be7bdb0
Draymonders <Draymonders@gmail.com>
2020-09-28 15:01:56 +0800

    update collection code view

--- src/main/java/demo/collection/list/ListMain.java
+++ src/main/java/demo/collection/list/ListMain.java
@@ -1,7 +1,9 @@
 package demo.collection.list;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @Description:
@@ -10,8 +12,19 @@ import java.util.List;
  */
 public class ListMain {
 
+  /**
+   * 给定 list，表示 id，可能重复。要求用 stream 实现 list -> map 的转换，map 统计 id 出现的次数
+   */
+  public static void countWord() {
+    List<String> list = List.of("1", "2", "3", "1", "6", "4", "6");
+
+    Map<String, Long> map = list.stream()
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    
+    System.out.println(map);
+  }
+
   public static void main(String[] args) {
-    List<String> arr = new ArrayList<>();
-    System.out.println(arr);
+    countWord();
   }
 }
