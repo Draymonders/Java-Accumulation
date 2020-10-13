346eae00a5e15a0a8a180fca6b20587d08675b18
draymonders <ixiaobing888@qq.com>
2020-10-13 08:54:15 +0800

    update try-with-resource

--- src/main/java/demo/api/file/FileService.java
+++ src/main/java/demo/api/file/FileService.java
@@ -12,25 +12,73 @@ import java.io.IOException;
  */
 public class FileService {
 
-  public void consumeFileContent(String url, FileConsumer fileConsumer)
-      throws IOException {
+  public void newConsumeFileContent(String url, FileConsumer fileConsumer) {
     File file = new File(url);
-    BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(file));
-
-    StringBuilder sb = new StringBuilder();
-    byte[] buffer = new byte[1024];
-    int readAvaiable = -1;
-    while ((readAvaiable = bufferedInputStream.read(buffer, 0, 1024)) != -1) {
-      String line = new String(buffer, 0, readAvaiable);
-      sb.append(line + "\n");
+    try (
+        FileInputStream fileInputStream = new FileInputStream(file);
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+    ) {
+      StringBuilder sb = new StringBuilder();
+      byte[] buffer = new byte[1024];
+      int readAvaiable = -1;
+      while ((readAvaiable = bufferedInputStream.read(buffer, 0, 1024)) != -1) {
+        String line = new String(buffer, 0, readAvaiable);
+        sb.append(line + "\n");
+      }
+      String content = sb.toString();
+      fileConsumer.consumeContent(content);
+    } catch (IOException e) {
+      System.out.println("error");
     }
-    String content = sb.toString();
-    fileConsumer.consumeContent(content);
   }
 
-  public static void main(String[] args) throws IOException {
+  public void oldConsumeFileContent(String url, FileConsumer fileConsumer) {
+    File file = new File(url);
+    FileInputStream fileInputStream = null;
+
+    BufferedInputStream bufferedInputStream = null;
+    try {
+      fileInputStream = new FileInputStream(file);
+      bufferedInputStream = new BufferedInputStream(fileInputStream);
+
+      StringBuilder sb = new StringBuilder();
+      byte[] buffer = new byte[1024];
+      int readAvaiable = -1;
+      while ((readAvaiable = bufferedInputStream.read(buffer, 0, 1024)) != -1) {
+        String line = new String(buffer, 0, readAvaiable);
+        sb.append(line + "\n");
+      }
+      String content = sb.toString();
+      fileConsumer.consumeContent(content);
+    } catch (IOException e) {
+      System.out.println("error");
+    } finally {
+      if (bufferedInputStream != null) {
+        try {
+          bufferedInputStream.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      if (fileInputStream != null) {
+        try {
+          fileInputStream.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+
+  public static void main(String[] args) {
     FileService fileService = new FileService();
-    fileService.consumeFileContent(
+    fileService.oldConsumeFileContent(
+        "/home/draymonder/Desktop/practice/Java-Accumulation/src/main/java/demo/api/file/FileConsumer.java",
+        System.out::println);
+
+    System.out.println("====");
+
+    fileService.newConsumeFileContent(
         "/home/draymonder/Desktop/practice/Java-Accumulation/src/main/java/demo/api/file/FileConsumer.java",
         System.out::println);
   }
