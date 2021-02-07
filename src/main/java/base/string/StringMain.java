803b147469275466430cef7af36889f56e762920
draymonders <ixiaobing888@qq.com>
2021-02-07 22:21:01 +0800

    update base module

---
+++ src/main/java/base/string/StringMain.java
@@ -0,0 +1,14 @@
+package base.string;
+
+/**
+ * String demo
+ *
+ * @author Draymonder
+ * @date 2021/02/03
+ */
+public class StringMain {
+
+  public static void main(String[] args) {
+    String s = new String("abc");
+  }
+}
