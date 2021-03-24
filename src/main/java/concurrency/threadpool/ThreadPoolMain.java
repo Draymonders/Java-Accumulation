2f670fee598629004bb1f5a68d354193ce0a19b6
Draymonders <Draymonders@gmail.com>
2021-03-24 15:03:55 +0800

    update thread pool test exception

--- src/main/java/concurrency/threadpool/ThreadPoolMain.java
+++ src/main/java/concurrency/threadpool/ThreadPoolMain.java
@@ -1,6 +1,11 @@
 package concurrency.threadpool;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * @author Draymonder
@@ -8,9 +13,21 @@ import java.util.concurrent.Executors;
  */
 public class ThreadPoolMain {
 
-  public static void main(String[] args) {
-    Executors.newFixedThreadPool(2);
-    Executors.newCachedThreadPool();
-    Executors.newSingleThreadExecutor();
+  public static void main(String[] args) throws ExecutionException, InterruptedException {
+    ExecutorService executor = Executors.newFixedThreadPool(1);
+    List<Future<?>> futureList = new ArrayList<>();
+    int n = 2;
+    for (int i = 0; i < n; i++) {
+      Future<?> future = executor.submit(() -> {
+        System.out.println(Thread.currentThread().getName());
+        int v = 5 / 0;
+        System.out.println(Thread.currentThread().getName() + " " + v);
+      });
+      futureList.add(future);
+    }
+    for (Future<?> future : futureList) {
+      future.get();
+    }
+    executor.shutdown();
   }
 }
